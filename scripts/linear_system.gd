df602e27dba6bbb161552db6b9ac441083f78ff0
Yassen Efremov <48584741+YassenEfremov@users.noreply.github.com>
2024-02-15 13:01:42 +0200

    Add detection for row addition/subtraction

--- scripts/linear_system.gd
+++ scripts/linear_system.gd
@@ -8,46 +8,29 @@ var selected_row: Row
 var selected_slot
 var slot_distances = {}
 var closest_slot
+var swap: bool = true
+var decided: bool = false
 
-func _process(delta):
-	if selected_row:
-		for row_slot in $Rows.get_children():
-			slot_distances[row_slot] = row_slot.global_position.distance_to(selected_row.global_position)
-		var new_closest = slot_distances.find_key(slot_distances.values().min())
-		print(slot_distances.values().min())
-		#if not closest_slot:
-			#closest_slot = selected_slot
-		if slot_distances.values().min() > 20 and new_closest != closest_slot:
-			if closest_slot.get_index() > new_closest.get_index():
-				# Shift all rows BELOW the closest slot
-				#print("shifting DOWN from ", new_closest.get_index())
-				shift_down(new_closest.get_index())
-			elif closest_slot.get_index() < new_closest.get_index():
-				# Shift all rows ABOVE the closest slot
-				#print("shifting UP from ", new_closest.get_index())
-				shift_up(new_closest.get_index())
-			closest_slot = new_closest
 
 func shift_down(i):
 	if i == number_of_rows - 1: return
-	if not $Rows.get_child(i + 1).get_children().size() <= 2:	# BAD CODE!!!
+	if $Rows.get_child(i + 1).get_children().size() > 2:	# BAD CODE!!!
 		shift_down(i + 1)
-	else:
-		#print("moving ", i, " -> ", i + 1)
-		move_row(i, i + 1)
+	#print("moving ", i, " -> ", i + 1)
+	move_row(i, i + 1)
 
 func shift_up(i):
 	if i == 0: return
-	if not $Rows.get_child(i - 1).get_children().size() <= 2:	# BAD CODE!!!
-		shift_down(i - 1)
-	else:
-		#print("moving ", i, " -> ", i - 1)
-		move_row(i, i - 1)
+	if $Rows.get_child(i - 1).get_children().size() > 2:	# BAD CODE!!!
+		shift_up(i - 1)
+	#print("moving ", i, " -> ", i - 1)
+	move_row(i, i - 1)
 
 func move_row(from_i: int, to_i: int):
 	var tween = get_tree().create_tween()
 	$Rows.get_child(from_i).get_child(2).reparent($Rows.get_child(to_i))
 	tween.tween_property($Rows.get_child(to_i).get_child(2), "position", Vector2.ZERO, 0.2).set_ease(Tween.EASE_OUT)
+	#print(from_i, " -> ", to_i)
 
 func row_picked_up(row: Row, slot):
 	selected_row = row
@@ -56,19 +39,69 @@ func row_picked_up(row: Row, slot):
 	
 	selected_row.reparent($Dragging)
 
+func on_dragged(row: Row):
+	for row_slot in $Rows.get_children():
+		slot_distances[row_slot] = row_slot.global_position.distance_to(selected_row.global_position)
+	var new_closest = slot_distances.find_key(slot_distances.values().min())
+	
+	if decided:
+		if swap:
+			if new_closest != closest_slot:
+				if closest_slot.get_index() > new_closest.get_index():
+					# Shift all rows BELOW the closest slot
+					if selected_row.position.y < new_closest.position.y and $Rows.get_child(new_closest.get_index()).get_children().size() > 2:
+						#print("shifting DOWN from ", new_closest.get_index())
+						shift_down(new_closest.get_index())
+					elif $Rows.get_child(new_closest.get_index() + 1).get_children().size() > 2:
+						shift_down(new_closest.get_index() + 1)
+				elif closest_slot.get_index() < new_closest.get_index():
+					# Shift all rows ABOVE the closest slot
+					if selected_row.position.y > new_closest.position.y and $Rows.get_child(new_closest.get_index()).get_children().size() > 2:
+						#print("shifting UP from ", new_closest.get_index())
+						shift_up(new_closest.get_index())
+					elif $Rows.get_child(new_closest.get_index() - 1).get_children().size() > 2:
+						shift_up(new_closest.get_index() - 1)
+				closest_slot = new_closest
+		elif slot_distances.find_key(slot_distances.values().min()).get_children().size() > 2:		# BAD CODE!!!
+			slot_distances.find_key(slot_distances.values().min()).modulate = Color.DODGER_BLUE
+			swap = true
+		
+		decided = false
+	else:
+		new_closest.modulate = Color.WHITE
+
 func row_dropped(row: Row):
 	selected_row = null
 	
-	var tween = get_tree().create_tween()
 	#if closest_slot.get_children().size() <= 2:		# BAD CODE!!!
 		#row.reparent(closest_slot)
 	#else:
 		#row.reparent(selected_slot)
 	#closest_slot = null
-	
-	# This works better
-	for r in $Rows.get_children():
-		if r.get_children().size() <= 2:
-			row.reparent(r)
-	
-	tween.tween_property(row, "position", Vector2.ZERO, 0.2).set_ease(Tween.EASE_OUT)
+	if swap:
+		# This works better
+		for r in $Rows.get_children():
+			if r.get_children().size() <= 2:
+				row.reparent(r)
+		
+		var tween = get_tree().create_tween()
+		tween.tween_property(row, "position", Vector2.ZERO, 0.2).set_ease(Tween.EASE_OUT)
+	else:
+		# add/subtract here
+		for r in $Rows.get_children():
+			if r.get_children().size() <= 2:
+				row.reparent(r)
+				print("reparented to row ", r.get_index())
+		
+		var tween = get_tree().create_tween()
+		tween.tween_property(row, "position", Vector2.ZERO, 0.2).set_ease(Tween.EASE_OUT)
+		
+		swap = true
+	slot_distances.find_key(slot_distances.values().min()).modulate = Color.WHITE
+
+
+func on_decided():	# Gets spammed for some reason?
+	if slot_distances.values().min() < 20:
+		swap = false
+	decided = true
+	print("decided")
--- scripts/row.gd
+++ scripts/row.gd
@@ -4,12 +4,14 @@ class_name Row extends Node2D
 @export var values: Array[int]
 
 var touched: bool = false
-var initialPos: Vector2
 var offset: Vector2
+var prev_pos: Vector2
+var t: Timer
 
 signal picked_up(row: Row, slot)
+signal dragged(row: Row)
 signal dropped(row: Row)
-#signal dropped
+signal decided
 
 
 func _ready():
@@ -17,19 +19,24 @@ func _ready():
 	$Label2.text = str(values[1])
 	$Label3.text = str(values[2])
 	$Label4.text = str(values[3])
+	t = $Timer
+	t.timeout.connect(func(): decided.emit())
 
 func _unhandled_input(event):
 	# Dragging is done here because the other method stops being called if you drag too fast
 	if event is InputEventScreenDrag and touched:
 		global_position = event.position + offset
+		dragged.emit(self)
+		
+		t.start(t.wait_time)
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventScreenTouch:
 		if event.pressed:
 			picked_up.emit(self, get_parent())
-			initialPos = global_position
 			offset = global_position - event.position
 			touched = true
-		else:
+		elif touched:
 			dropped.emit(self)
+			t.stop()
 			touched = false
