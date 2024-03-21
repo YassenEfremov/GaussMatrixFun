055d18451589da59c85fa059afb2545e05cd9a70
Yassen Efremov <48584741+YassenEfremov@users.noreply.github.com>
2024-03-21 10:52:17 +0200

    Add animation on found solution

--- scripts/solution.gd
+++ scripts/solution.gd
@@ -9,5 +9,8 @@ extends Control
 
 var solution: float:
 	set(value):
+		$Value.scale = Vector2(3, 3)
 		solution = value
 		$Value.text = str(solution)
+		var tween = get_tree().create_tween()
+		tween.tween_property($Value, "scale", Vector2.ONE, 0.2)
